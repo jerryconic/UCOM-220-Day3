06d6b8012cfcb84ac7c5c10d9d68a3f0bef51b21
Jerry Huang <jerry@conic-soft.com>
2024-04-24 09:37:50 +0800

    兌換外幣

--- Ap01/Form1.Designer.cs
+++ Ap01/Form1.Designer.cs
@@ -28,13 +28,109 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.cmbCurr = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNT = new System.Windows.Forms.TextBox();
+            this.btnExchange = new System.Windows.Forms.Button();
+            this.txtAmount = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmbCurr
+            // 
+            this.cmbCurr.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbCurr.FormattingEnabled = true;
+            this.cmbCurr.Items.AddRange(new object[] {
+            "美金",
+            "日圓",
+            "歐元"});
+            this.cmbCurr.Location = new System.Drawing.Point(122, 57);
+            this.cmbCurr.Name = "cmbCurr";
+            this.cmbCurr.Size = new System.Drawing.Size(162, 24);
+            this.cmbCurr.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(62, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "幣別:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(62, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "台幣:";
+            // 
+            // txtNT
+            // 
+            this.txtNT.Location = new System.Drawing.Point(122, 97);
+            this.txtNT.Name = "txtNT";
+            this.txtNT.Size = new System.Drawing.Size(162, 27);
+            this.txtNT.TabIndex = 3;
+            // 
+            // btnExchange
+            // 
+            this.btnExchange.Location = new System.Drawing.Point(122, 139);
+            this.btnExchange.Name = "btnExchange";
+            this.btnExchange.Size = new System.Drawing.Size(106, 31);
+            this.btnExchange.TabIndex = 4;
+            this.btnExchange.Text = "兌換";
+            this.btnExchange.UseVisualStyleBackColor = true;
+            this.btnExchange.Click += new System.EventHandler(this.btnExchange_Click);
+            // 
+            // txtAmount
+            // 
+            this.txtAmount.Location = new System.Drawing.Point(122, 185);
+            this.txtAmount.Name = "txtAmount";
+            this.txtAmount.Size = new System.Drawing.Size(162, 27);
+            this.txtAmount.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(14, 188);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "可兌換金額:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(360, 253);
+            this.Controls.Add(this.txtAmount);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnExchange);
+            this.Controls.Add(this.txtNT);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmbCurr);
+            this.Font = new System.Drawing.Font("新細明體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "兌換外幣";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox cmbCurr;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNT;
+        private System.Windows.Forms.Button btnExchange;
+        private System.Windows.Forms.TextBox txtAmount;
+        private System.Windows.Forms.Label label3;
     }
 }
 
--- Ap01/Form1.cs
+++ Ap01/Form1.cs
@@ -16,5 +16,28 @@ namespace Ap01
         {
             InitializeComponent();
         }
+
+        private void btnExchange_Click(object sender, EventArgs e)
+        {
+            int nt;
+            decimal amount;
+            int.TryParse(txtNT.Text, out nt);
+            switch(cmbCurr.Text)
+            {
+                case "美金":
+                    amount = nt / 30.05m;
+                    break;
+                case "日圓":
+                    amount = nt / 0.2756M;
+                    break;
+                case "歐元":
+                    amount = nt / 32.15m;
+                    break;
+                default:
+                    amount = 0;
+                    break;
+            }
+            txtAmount.Text = amount.ToString("#,##0.00");
+        }
     }
 }
